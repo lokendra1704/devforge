5bc3c5962a37ac47fd5fe66b65a60448132472cb
Lokendra Bairwa <lokendra@talkshopclub.com>
2026-06-17 19:06:42 +0530

    Fix: Re-add rnn-to-transformers to index after merge

--- src/data/index.ts
+++ src/data/index.ts
@@ -37,6 +37,7 @@ import vlJepa from './vl-jepa'
 import leWorldModel from './le-world-model'
 import { leJepa } from './le-jepa'
 import { lejepaiIdentifiability } from './lejepa-identifiability'
+import rnnToTransformers from './rnn-to-transformers'
 
 export const SUBJECTS: Subject[] = [
   dsa,
@@ -54,7 +55,7 @@ export const SUBJECTS: Subject[] = [
 ]
 
 /** Research papers, onboarded the same way as books but surfaced in their own section. */
-export const PAPERS: Subject[] = [backpropagation, codeAsAgentHarness, clawvm, sira, intrinsicDimension, lora, agenticAiAdaptation, craftCreativeGeneration, skillOpt, claudeCodeArchitecture, lossLandscapeAnalysis, mctsOptions, monteCarloTreeDiffusion, dta, deepQNetworks, phoneHarness, perceptron, lenetGtn, iJepa, varJepa, vjepa2, vlJepa, leWorldModel, leJepa, lejepaiIdentifiability]
+export const PAPERS: Subject[] = [backpropagation, codeAsAgentHarness, clawvm, sira, intrinsicDimension, lora, agenticAiAdaptation, craftCreativeGeneration, skillOpt, claudeCodeArchitecture, lossLandscapeAnalysis, mctsOptions, monteCarloTreeDiffusion, dta, deepQNetworks, phoneHarness, perceptron, lenetGtn, iJepa, varJepa, vjepa2, vlJepa, leWorldModel, leJepa, lejepaiIdentifiability, rnnToTransformers]
 
 /** Industry whitepapers, onboarded the same way as books but surfaced in their own section. */
 export const WHITEPAPERS: Subject[] = [vibeCodingSdlc]
